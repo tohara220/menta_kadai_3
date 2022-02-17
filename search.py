509c30739ef88dea32769eb93f14e7be258dc623
tohara220 <tohara.220@gmail.com>
2022-02-18 06:29:22 +0900

    fix_csv_isexists

--- search.py
+++ search.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import eel
+import os
 
 ### デスクトップアプリ作成課題
 def kimetsu_search(word, csv_name):
+    # CSV存在判定
+    if not os.path.exists(csv_name):
+        with open(csv_name, mode="w", encoding="utf-8_sig") as f:
+            f.write(",name\n0,たんじろう\n1,ねずこ\n2,ぎゆう\n3,いのすけ\n4,かんろじ\n5,かがや")
     # 検索対象取得
     df=pd.read_csv(f"./{csv_name}")
     source=list(df["name"])
-
     # 検索
     if word in source:
         eel.view_log_js(f"『{word}』はあります")
