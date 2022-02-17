589061f408a84d0e278978930a2613a311139885
tohara220 <tohara.220@gmail.com>
2022-02-18 07:12:30 +0900

    fix_csv_isexist2

--- search.py
+++ search.py
@@ -6,8 +6,7 @@ import os
 def kimetsu_search(word, csv_name):
     # CSV存在判定
     if not os.path.exists(csv_name):
-        with open(csv_name, mode="w", encoding="utf-8_sig") as f:
-            f.write(",name\n0,たんじろう\n1,ねずこ\n2,ぎゆう\n3,いのすけ\n4,かんろじ\n5,かがや")
+        source = ["たんじろう", "ねずこ", "ぎゆう", "いのすけ", "かんろじ", "かがや"]
     # 検索対象取得
     df=pd.read_csv(f"./{csv_name}")
     source=list(df["name"])
